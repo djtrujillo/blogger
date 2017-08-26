8d82c032e06e7a007358fd4c22a2d883e01ba446
djtrujillo <danny.j.trujillo@gmail.com>
2017-08-26 10:28:03 -0600

    Tags controller and view

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,17 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    @tag.destroy
+
+    flash.notice = "Tag '#{@tag.name}' DESTROYED!"
+
+
+    redirect_to tags_path
+  end
 end
